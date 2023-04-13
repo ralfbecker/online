192503147d31693c88e296e5463db6092cfc8b13
Szymon Kłos <szymon.klos@collabora.com>
2023-04-13 11:18:10 +0200

    jsdialog: a11y: add aria properties for tree / listbox
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I190929780783845b69c518fc9fe549e1a841cb9c

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -3124,7 +3124,8 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			&& data.type !== 'frame'
 			&& data.type !== 'expander'
 			&& data.type !== 'panel'
-			&& data.type !== 'buttonbox')
+			&& data.type !== 'buttonbox'
+			&& data.type !== 'treelistbox')
 			control.setAttribute('tabIndex', '0');
 	},
 
